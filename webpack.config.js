71f775bdfbe4aad6673eb9db9510394500c7293d
Nicolas Vauche <hello@nicolasvauche.net>
2022-11-24 13:52:00 +0000

    Import Home.js

--- webpack.config.js
+++ webpack.config.js
@@ -25,12 +25,6 @@ Encore
     pattern: /\.(png|jpg|webp)$/
   })
 
-  .copyFiles({
-    from: './assets/js',
-    to: 'js/[path][name].[ext]',
-    pattern: /\.(js)$/
-  })
-
   /*
    * ENTRY CONFIG
    *
